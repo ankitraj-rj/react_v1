6b500632b62cbc778b38be2542d1e81b14c094ef
Ankit Raj <contactankitrajofficial@gmail.com>
2026-07-23 21:03:39 +0530

    add import statment

--- ch-two/App.js
+++ ch-two/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const h1 = React.createElement("h1", { id: "heading" }, "Hellow World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);
