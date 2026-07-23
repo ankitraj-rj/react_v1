64e664fc1947f96f9239f45e8e00f766ae7bceca
Ankit Raj <contactankitrajofficial@gmail.com>
2026-07-23 20:42:17 +0530

    add CH-2 initial code

---
+++ ch-two/App.js
@@ -0,0 +1,3 @@
+const h1 = React.createElement("h1", { id: "heading" }, "Hellow World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
---
+++ ch-two/dist/ch-two.6bd02f5a.js
@@ -0,0 +1,7 @@
+const h1 = React.createElement("h1", {
+    id: "heading"
+}, "Hellow World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
+
+//# sourceMappingURL=ch-two.6bd02f5a.js.map
