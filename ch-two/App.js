8fd7d08fa1ab85acd793fdaf28126d7b0edc3051
Ankit Raj <contactankitrajofficial@gmail.com>
2026-07-24 19:44:42 +0530

    add jsx code

--- ch-two/App.js
+++ ch-two/App.js
@@ -6,3 +6,9 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", null, "Hello ji");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+// JSX 
+
+const jsxHeading = <h1>Hello from jsx</h1>
+
+root.render(jsxHeading)
